63aaa913fdb701a99e4c1498fedfac680d99bfea
Shou1204 <flyingsheep0000@gmail.com>
2022-12-22 14:49:41 +0900

    投稿者以外が専用のページに遷移できないよう制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.all
@@ -48,4 +49,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    if authenticate_user!
+      redirect_to action: :index
+    end
+  end
+
 end
